04566fa10778932f1f7b5ede910d9c421c61650b
Git-Jek <119988051+Git-Jek@users.noreply.github.com>
2022-12-16 10:09:47 +0800

    Manual Testing for BMI Calculator.

--- New Assignment BMI/bmi_calculator.js
+++ New Assignment BMI/bmi_calculator.js
@@ -1,6 +1,20 @@
 function calcBMI(height,weight){
     let BMI = (weight / ((height * height) / 10000))
-    console.log(BMI);
+    return BMI;
 }
 
-calcBMI(177,80)
+console.log(calcBMI(177,80));
+
+// Manual Testing
+
+function testCalcBMI(){
+    const result = calcBMI(177, 80);
+    const expected = 25.535446391522232;
+
+    console.assert(
+        result === expected,
+        `The result ${result} doesn't match the expected value ${expected}.`
+    )
+}
+
+testCalcBMI();
